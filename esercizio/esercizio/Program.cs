690322e597915f429bf8c7c312bdf49a7185d60f
Erixviii <4079753@itisrossi.vi.it>
2021-11-20 09:26:54 +0100

    Update Program.cs

--- esercizio/esercizio/Program.cs
+++ esercizio/esercizio/Program.cs
@@ -7,6 +7,7 @@ namespace esercizio1
     {
         static void Main(string[] args)
         {
+            //hey
             Persona eric = new Persona();
             WriteLine("prova_Classi.Persona");
             WriteLine(eric.ToString());
