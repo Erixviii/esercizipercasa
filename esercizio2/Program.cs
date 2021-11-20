2e3f62f48d5cda81d001cef742aeaae6849da673
Erixviii <4079753@itisrossi.vi.it>
2021-11-20 16:21:47 +0100

    es2

---
+++ esercizio2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace esercizio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
