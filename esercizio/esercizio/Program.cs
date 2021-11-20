0340e45e63ec85f2d8b69493942f782f0e56e300
Erixviii <94734941+Erixviii@users.noreply.github.com>
2021-11-20 08:25:16 +0100

    Add files via upload

---
+++ esercizio/esercizio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace esercizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Persona
+    {
+        private string _nome;
+        private string _cognome;
+        private int _eta;
+        private char _sesso;
+
+        public Persona()
+        {
+
+        }
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Cognome { get => _cognome; set => _cognome = value; }
+        public int Eta { get => _eta; set => {
+                if (value != 0 && int.TryParse(value, out _eta))
+                {
+                }
+                else
+                    MessageBox.Show("Error ") ;
+            } }
+        public char Sesso { get => _sesso; set => _sesso = value; }
+    }
+}
