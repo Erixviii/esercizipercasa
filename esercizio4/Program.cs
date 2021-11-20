e4c799dad65611b03d4566aaf63f1ad5f7964196
Erixviii <4079753@itisrossi.vi.it>
2021-11-20 17:56:52 +0100

    Update Program.cs

--- esercizio4/Program.cs
+++ esercizio4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace esercizio4
 {
@@ -6,7 +7,59 @@ namespace esercizio4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Potenza p = new Potenza();
+            WriteLine("Inserisci la prima base");
+            p.Base = int.Parse(ReadLine());
+            WriteLine("Inserisci la seconda base");
+            p.Base = int.Parse(ReadLine());
+        }
+    }
+
+    class Potenza
+    {
+        private int _base;
+        private int _esponente;
+
+        public Potenza()
+        {
+            Base = 0;
+            Esponente = 0;
+        }
+
+        public Potenza(int @base, int esponente)
+        {
+            Base = @base;
+            Esponente = esponente;
+        }
+
+        public int Base { get => _base; set
+            {
+                if (value > 0 && int.TryParse(value.ToString(), out _base)) { }
+                else
+                {
+                    _base = 1;
+                    WriteLine("valore errato, base negativa o non numerica");
+                }
+            }
+        }
+
+        public int Esponente
+        {
+            get => _esponente; set
+            {
+                if (value > 0 && int.TryParse(value.ToString(), out _esponente)) { }
+                else
+                {
+                    _esponente = 1;
+                    WriteLine("valore errato, esponente negativo o non numerico" +
+                        "");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $" {Base}^{Esponente} ";
         }
     }
 }
