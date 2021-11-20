f23034026e745ec8844c17cdee735f6f6c16ab5c
Erixviii <4079753@itisrossi.vi.it>
2021-11-20 09:23:54 +0100

    fatto l'esercizio in 1 secondo ;)

--- esercizio/esercizio/Program.cs
+++ esercizio/esercizio/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using static System.Console;
 
-namespace esercizio
+namespace esercizio1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona eric = new Persona();
+            WriteLine("prova_Classi.Persona");
+            WriteLine(eric.ToString());
+            WriteLine("prova_Classi.Persona");
+            WriteLine("Ora utilizzo la proprietà per modificare il nome");
+            eric.Nome = "";
+            WriteLine($"Nuovo nome: {eric.Nome}");
+            eric.Nome = "Eric";
+            WriteLine($"Nuovo nome: {eric.Nome}");
+            eric.Eta = 120;
+            WriteLine($"eta: {eric.Eta}");
+            eric.Eta = 0;
+            WriteLine($"eta: {eric.Eta}");
+            eric.Eta = -5;
+            WriteLine($"eta: {eric.Eta}");
+            eric.Eta = 20;
+            WriteLine($"eta: {eric.Eta}");
+            eric.StampaMessaggio();
+            eric.Saluti();
         }
     }
 
@@ -15,22 +34,51 @@ namespace esercizio
         private string _nome;
         private string _cognome;
         private int _eta;
-        private char _sesso;
+        private string _sesso;
 
         public Persona()
         {
-
+            _nome = "";
+            _cognome = "";
+            _eta = 0;
+            _sesso = "";
         }
 
-        public string Nome { get => _nome; set => _nome = value; }
-        public string Cognome { get => _cognome; set => _cognome = value; }
-        public int Eta { get => _eta; set => {
-                if (value != 0 && int.TryParse(value, out _eta))
+        public string Nome { get => _nome; set { if (value.Trim().Equals("")) { _nome = "(nessun nome)"; } } }
+        public string Cognome { get => _cognome; set { if (value.Trim().Equals("")) { _cognome = "(nessun nome)"; } } }
+        public int Eta
+        {
+            get => _eta; set
+            {
+                if (value > 0 && value <= 110 && int.TryParse(value.ToString(), out _eta))
                 {
+
                 }
                 else
-                    MessageBox.Show("Error ") ;
-            } }
-        public char Sesso { get => _sesso; set => _sesso = value; }
+                {
+                    _eta = 1;
+                    WriteLine("valore errato, dato non modificato ");
+                }
+            }
+        }
+        public string Sesso { get => _sesso; set => _sesso = value; }
+
+        public void StampaMessaggio()
+        {
+            WriteLine("Ho stampato quanto richiesto");
+        }
+
+        public void Saluti()
+        {
+            WriteLine("Ciao ciao");
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}\n" +
+                   $"Cognome: {Cognome}"
+                   ;
+        }
+
     }
 }
