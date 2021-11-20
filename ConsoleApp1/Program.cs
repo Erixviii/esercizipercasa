85778cb336f472e8808aab67f2c4c28e283e8c77
Erixviii <4079753@itisrossi.vi.it>
2021-11-20 16:50:25 +0100

    es3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace esercizio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
