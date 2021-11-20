328bda1d4c3a7875452deac4d1f5aa1afcdef112
Erixviii <4079753@itisrossi.vi.it>
2021-11-20 16:14:01 +0100

    Revert "Update Program.cs"
    This reverts commit 690322e597915f429bf8c7c312bdf49a7185d60f.

--- esercizio/esercizio/Program.cs
+++ esercizio/esercizio/Program.cs
@@ -7,7 +7,6 @@ namespace esercizio1
     {
         static void Main(string[] args)
         {
-            //hey
             Persona eric = new Persona();
             WriteLine("prova_Classi.Persona");
             WriteLine(eric.ToString());
