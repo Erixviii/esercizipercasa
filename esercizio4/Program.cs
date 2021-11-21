dda3f8e0d7530b7c6c08644511fbde0331bb78a2
Erixviii <4079753@itisrossi.vi.it>
2021-11-21 16:00:12 +0100

    es4

--- esercizio4/Program.cs
+++ esercizio4/Program.cs
@@ -7,14 +7,84 @@ namespace esercizio4
     {
         static void Main(string[] args)
         {
-            Potenza p = new Potenza();
+            Potenza p1 = new Potenza();
             WriteLine("Inserisci la prima base");
-            p.Base = int.Parse(ReadLine());
+            int risposta;
+            while (!int.TryParse(ReadLine(), out risposta))
+            {
+                WriteLine("Inserisci un valore corretto");
+            }
+            p1.Base = risposta;
+            WriteLine("Inserisci il primo esponente");
+            while (!int.TryParse(ReadLine(), out risposta))
+            {
+                WriteLine("Inserisci un valore corretto");
+            }
+            p1.Esponente = risposta;
+
+            Potenza p2 = new Potenza();
             WriteLine("Inserisci la seconda base");
-            p.Base = int.Parse(ReadLine());
+            while (!int.TryParse(ReadLine(), out risposta))
+            {
+                WriteLine("Inserisci un valore corretto");
+            }
+            p2.Base = risposta;
+            WriteLine("Inserisci il secondo esponente");
+            while (!int.TryParse(ReadLine(), out risposta))
+            {
+                WriteLine("Inserisci un valore corretto");
+            }
+            p2.Esponente = risposta;
+
+            WriteLine(p1.ToString() + " * " + p2.ToString() + " = " + Op_asterisco(p1, p2).ToString());
+
+            Potenza Op_asterisco(Potenza p1,Potenza p2)
+            {
+                Potenza p3 = new Potenza(p1.Base, p1.Esponente);
+
+                if (p1.Base == p2.Base)
+                {
+
+                    p3.Esponente = p1.Esponente + p2.Esponente;
+                }
+                else
+                {
+                    p3.Base = (p1.Base ^ (p1.Esponente)) * (p2.Base ^ (p2.Esponente));
+                    p3.Esponente = 1;
+                }
+
+                if (p1.Esponente == p2.Esponente)
+                {
+
+                    p3.Base = p1.Base * p2.Base;
+                    p3.Esponente = p1.Esponente;
+                }
+
+                return p3;
+            }
+
+            Potenza Op_slash(Potenza p1, Potenza p2)
+            {
+                Potenza p3 = new Potenza(p1.Base, p1.Esponente);
+
+                if (p1.Base == p2.Base)
+                {
+
+                    p3.Esponente = p1.Esponente - p2.Esponente;
+                }
+
+                if (p1.Esponente == p2.Esponente)
+                {
+
+                    p3.Base = p1.Base / p2.Base;
+                    p3.Esponente = p1.Esponente;
+                }
+
+                return p3;
+            }
         }
     }
-
+    
     class Potenza
     {
         private int _base;
@@ -34,32 +104,51 @@ namespace esercizio4
 
         public int Base { get => _base; set
             {
-                if (value > 0 && int.TryParse(value.ToString(), out _base)) { }
+                if (value >= 0 ) { _base = value; }
                 else
                 {
                     _base = 1;
-                    WriteLine("valore errato, base negativa o non numerica");
+                    WriteLine("valore errato, base negativa");
                 }
             }
+        }
+
+        public static Potenza Moltiplica(Potenza p1, Potenza p2)
+        {
+            Potenza p3 = new Potenza(p1.Base, p1.Esponente);
+
+            if (p1.Base == p2.Base)
+            {
+
+                p3.Esponente = p1.Esponente + p2.Esponente;
+            }
+
+            if (p1.Esponente == p2.Esponente)
+            {
+
+                p3.Base = p1.Base * p2.Base;
+                p3.Esponente = p1.Esponente;
+            }
+
+            return p3;
         }
 
         public int Esponente
         {
             get => _esponente; set
             {
-                if (value > 0 && int.TryParse(value.ToString(), out _esponente)) { }
+                if (value >= 0) { _esponente = value; }
                 else
                 {
                     _esponente = 1;
-                    WriteLine("valore errato, esponente negativo o non numerico" +
-                        "");
+                    WriteLine("valore errato, esponente negativo");
                 }
             }
         }
 
         public override string ToString()
         {
-            return $" {Base}^{Esponente} ";
+            return $"{Base}^{Esponente}";
         }
     }
 }
