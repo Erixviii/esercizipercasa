885186c8c40872e8a2a3f14cf100b8da8191e756
Erixviii <4079753@itisrossi.vi.it>
2021-11-23 12:36:24 +0100

    inizio es1

---
+++ esercizio1 .NET Framework/esercizio1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace esercizio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Cfrazione
+    {
+        private int _numeratore;
+        private int _denominatore;
+
+        public Cfrazione()
+        {
+            _numeratore=0;
+            _denominatore=0;
+        }
+
+        public Cfrazione(int numeratore, int denominatore)
+        {
+            Numeratore = numeratore;
+            Denominatore = denominatore;
+        }
+
+        public int Numeratore { get => _numeratore; set => _numeratore = value; }
+        public int Denominatore { get => _denominatore; set => _denominatore = value; }
+    }
+}
