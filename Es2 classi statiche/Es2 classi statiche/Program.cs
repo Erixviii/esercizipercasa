c072e4a2d075080bfd4c0e129145b53365c405d5
Erixviii <4079753@itisrossi.vi.it>
2021-12-11 08:54:31 +0100

    es2 classi statiche

---
+++ Es2 classi statiche/Es2 classi statiche/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Es2_classi_statiche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RandomDouble.Next());
+        }
+    }
+
+    static class RandomDouble
+    {
+        private static Random num;
+
+        public static double Next()
+        {
+
+            num = new Random();
+            return num.NextDouble();
+        }
+    }
+}
