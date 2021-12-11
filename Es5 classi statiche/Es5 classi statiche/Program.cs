d19e904d9b633a23e3fccfc1b17a927805c99116
Erixviii <4079753@itisrossi.vi.it>
2021-12-11 09:34:20 +0100

    es5 classi statiche

--- Es5 classi statiche/Es5 classi statiche/Program.cs
+++ Es5 classi statiche/Es5 classi statiche/Program.cs
@@ -6,7 +6,21 @@ namespace Es5_classi_statiche
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Convertitore.ItoBin(8));
+        }
+    }
+
+    static class Convertitore
+    {
+        public static string ItoBin(int i)
+        {
+            return Convert.ToString(i, 2);
         }
     }
 }
+//CONVERTITORE Si definisca una classe statica Convertitore che senza bisogno
+//di creare istanze, definisca metodi statici che rappresentano: 1.Conversione da intero(sistema decimale) a intero
+//(sistema binario) 2.Conversione da intero(sistema decimale) a intero(sistema ottale) 3.Conversione da intero
+//(sistema decimale) a intero(sistema esadecimale) 4.Conversione da intero(sistema binario) a intero(sistema
+//decimale) 5.Conversione da intero(sistema ottale) a intero(sistema decimale) 6.Conversione da intero(sistema
+//esadecimale) a intero(sistema decimale)
