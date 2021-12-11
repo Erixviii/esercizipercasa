8ad59092bec9120a593df1c0f90711236f7dffe6
Erixviii <4079753@itisrossi.vi.it>
2021-12-11 08:49:39 +0100

    es 1 classi statiche

---
+++ Es1 classi statiche/Es1 classi statiche/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Es1_classi_statiche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RandomInt.Next());
+            Console.WriteLine(RandomInt.Next(100));
+            Console.WriteLine(RandomInt.Next(0,10));
+        }
+    }
+
+    static class RandomInt
+    {
+        private static Random num;
+
+        public static int Next()
+        {
+
+            num = new Random();
+            return num.Next();
+        }
+        public static int Next(int maxValue)
+        {
+
+            num = new Random();
+            return num.Next(maxValue);
+        }
+        public static int Next(int minValue, int maxValue)
+        {
+
+            num = new Random();
+            return num.Next(minValue,maxValue);
+        }
+    }
+}
