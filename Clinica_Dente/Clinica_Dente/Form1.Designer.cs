d5ba2c1219c6c659344c75f7bc345dd5dd22ba68
salvy-greek <4079753@itisrossi.vi.it>
2021-12-29 12:03:00 +0100

    continuazione es
    manca il filtro e ho finito

--- Clinica_Dente/Clinica_Dente/Form1.Designer.cs
+++ Clinica_Dente/Clinica_Dente/Form1.Designer.cs
@@ -28,13 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPazienti = new System.Windows.Forms.TabPage();
+            this.DGVPazienti = new System.Windows.Forms.DataGridView();
+            this.tabMedici = new System.Windows.Forms.TabPage();
+            this.DGVMedici = new System.Windows.Forms.DataGridView();
+            this.tabPatologie = new System.Windows.Forms.TabPage();
+            this.DGVPatologie = new System.Windows.Forms.DataGridView();
+            this.tabSpecializzazioni = new System.Windows.Forms.TabPage();
+            this.DGVSpecializzazioni = new System.Windows.Forms.DataGridView();
+            this.tabAppuntamenti = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.DGVAppuntamenti = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPazienti.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPazienti)).BeginInit();
+            this.tabMedici.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVMedici)).BeginInit();
+            this.tabPatologie.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPatologie)).BeginInit();
+            this.tabSpecializzazioni.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVSpecializzazioni)).BeginInit();
+            this.tabAppuntamenti.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVAppuntamenti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPazienti);
+            this.tabControl1.Controls.Add(this.tabMedici);
+            this.tabControl1.Controls.Add(this.tabPatologie);
+            this.tabControl1.Controls.Add(this.tabSpecializzazioni);
+            this.tabControl1.Controls.Add(this.tabAppuntamenti);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(934, 640);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPazienti
+            // 
+            this.tabPazienti.Controls.Add(this.DGVPazienti);
+            this.tabPazienti.Location = new System.Drawing.Point(4, 28);
+            this.tabPazienti.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPazienti.Name = "tabPazienti";
+            this.tabPazienti.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPazienti.Size = new System.Drawing.Size(1051, 880);
+            this.tabPazienti.TabIndex = 0;
+            this.tabPazienti.Text = "Pazienti";
+            this.tabPazienti.UseVisualStyleBackColor = true;
+            // 
+            // DGVPazienti
+            // 
+            this.DGVPazienti.AllowUserToAddRows = false;
+            this.DGVPazienti.AllowUserToDeleteRows = false;
+            this.DGVPazienti.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVPazienti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVPazienti.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVPazienti.Location = new System.Drawing.Point(4, 4);
+            this.DGVPazienti.Name = "DGVPazienti";
+            this.DGVPazienti.ReadOnly = true;
+            this.DGVPazienti.Size = new System.Drawing.Size(1043, 872);
+            this.DGVPazienti.TabIndex = 0;
+            // 
+            // tabMedici
+            // 
+            this.tabMedici.Controls.Add(this.DGVMedici);
+            this.tabMedici.Location = new System.Drawing.Point(4, 28);
+            this.tabMedici.Name = "tabMedici";
+            this.tabMedici.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMedici.Size = new System.Drawing.Size(1051, 880);
+            this.tabMedici.TabIndex = 1;
+            this.tabMedici.Text = "Medici";
+            this.tabMedici.UseVisualStyleBackColor = true;
+            // 
+            // DGVMedici
+            // 
+            this.DGVMedici.AllowUserToAddRows = false;
+            this.DGVMedici.AllowUserToDeleteRows = false;
+            this.DGVMedici.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVMedici.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVMedici.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVMedici.Location = new System.Drawing.Point(3, 3);
+            this.DGVMedici.Name = "DGVMedici";
+            this.DGVMedici.ReadOnly = true;
+            this.DGVMedici.Size = new System.Drawing.Size(1045, 874);
+            this.DGVMedici.TabIndex = 1;
+            // 
+            // tabPatologie
+            // 
+            this.tabPatologie.Controls.Add(this.DGVPatologie);
+            this.tabPatologie.Location = new System.Drawing.Point(4, 28);
+            this.tabPatologie.Name = "tabPatologie";
+            this.tabPatologie.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPatologie.Size = new System.Drawing.Size(926, 608);
+            this.tabPatologie.TabIndex = 2;
+            this.tabPatologie.Text = "Patologie";
+            this.tabPatologie.UseVisualStyleBackColor = true;
+            // 
+            // DGVPatologie
+            // 
+            this.DGVPatologie.AllowUserToAddRows = false;
+            this.DGVPatologie.AllowUserToDeleteRows = false;
+            this.DGVPatologie.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVPatologie.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVPatologie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVPatologie.Location = new System.Drawing.Point(3, 3);
+            this.DGVPatologie.Name = "DGVPatologie";
+            this.DGVPatologie.ReadOnly = true;
+            this.DGVPatologie.Size = new System.Drawing.Size(920, 602);
+            this.DGVPatologie.TabIndex = 1;
+            // 
+            // tabSpecializzazioni
+            // 
+            this.tabSpecializzazioni.Controls.Add(this.DGVSpecializzazioni);
+            this.tabSpecializzazioni.Location = new System.Drawing.Point(4, 28);
+            this.tabSpecializzazioni.Name = "tabSpecializzazioni";
+            this.tabSpecializzazioni.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSpecializzazioni.Size = new System.Drawing.Size(1051, 880);
+            this.tabSpecializzazioni.TabIndex = 3;
+            this.tabSpecializzazioni.Text = "Specializzazioni";
+            this.tabSpecializzazioni.UseVisualStyleBackColor = true;
+            // 
+            // DGVSpecializzazioni
+            // 
+            this.DGVSpecializzazioni.AllowUserToAddRows = false;
+            this.DGVSpecializzazioni.AllowUserToDeleteRows = false;
+            this.DGVSpecializzazioni.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVSpecializzazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVSpecializzazioni.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVSpecializzazioni.Location = new System.Drawing.Point(3, 3);
+            this.DGVSpecializzazioni.Name = "DGVSpecializzazioni";
+            this.DGVSpecializzazioni.ReadOnly = true;
+            this.DGVSpecializzazioni.Size = new System.Drawing.Size(1045, 874);
+            this.DGVSpecializzazioni.TabIndex = 1;
+            // 
+            // tabAppuntamenti
+            // 
+            this.tabAppuntamenti.Controls.Add(this.tableLayoutPanel1);
+            this.tabAppuntamenti.Location = new System.Drawing.Point(4, 28);
+            this.tabAppuntamenti.Name = "tabAppuntamenti";
+            this.tabAppuntamenti.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAppuntamenti.Size = new System.Drawing.Size(926, 608);
+            this.tabAppuntamenti.TabIndex = 4;
+            this.tabAppuntamenti.Text = "Appuntamenti";
+            this.tabAppuntamenti.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.67391F));
+            this.tableLayoutPanel1.Controls.Add(this.DGVAppuntamenti, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.04984F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 64.95016F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(920, 602);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // DGVAppuntamenti
+            // 
+            this.DGVAppuntamenti.AllowUserToAddRows = false;
+            this.DGVAppuntamenti.AllowUserToDeleteRows = false;
+            this.DGVAppuntamenti.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVAppuntamenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVAppuntamenti.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVAppuntamenti.Location = new System.Drawing.Point(3, 214);
+            this.DGVAppuntamenti.Name = "DGVAppuntamenti";
+            this.DGVAppuntamenti.ReadOnly = true;
+            this.DGVAppuntamenti.Size = new System.Drawing.Size(914, 385);
+            this.DGVAppuntamenti.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(934, 640);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPazienti.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPazienti)).EndInit();
+            this.tabMedici.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVMedici)).EndInit();
+            this.tabPatologie.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPatologie)).EndInit();
+            this.tabSpecializzazioni.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVSpecializzazioni)).EndInit();
+            this.tabAppuntamenti.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVAppuntamenti)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPazienti;
+        private System.Windows.Forms.TabPage tabMedici;
+        private System.Windows.Forms.TabPage tabPatologie;
+        private System.Windows.Forms.TabPage tabSpecializzazioni;
+        private System.Windows.Forms.TabPage tabAppuntamenti;
+        private System.Windows.Forms.DataGridView DGVPazienti;
+        private System.Windows.Forms.DataGridView DGVMedici;
+        private System.Windows.Forms.DataGridView DGVPatologie;
+        private System.Windows.Forms.DataGridView DGVSpecializzazioni;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView DGVAppuntamenti;
     }
 }
 
--- Clinica_Dente/Clinica_Dente/Form1.cs
+++ Clinica_Dente/Clinica_Dente/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,330 @@ namespace Clinica_Dente
 {
     public partial class Form1 : Form
     {
+
+        private List<Paziente> LSTpazienti;
+        private List<Medico> LSTmedici;
+        private List<Patologia> LSTpatologie;
+        private List<Specializzazione> LSTspecializzazioni;
+        private List<Appuntamento> LSTappuntamenti;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public void PazientiDaFile()
+        {
+            
+            string[] pazienticsv = File.ReadAllLines("pazienti.csv");
+            foreach (string riga in pazienticsv)
+            {
+
+                int l = 0;
+                Paziente paziente = new Paziente();
+                for (int i = 0; i < 7; i++)
+                {
+
+                    string parola = "";
+                    while (riga[l].ToString() != ";")
+                    {
+                        parola += riga[l];
+                        l++;
+                    }
+
+                    switch (i)
+                    {
+                        case 0:
+                            paziente.Nome = parola;
+                            break;
+                        case 1:
+                            paziente.Cognome = parola;
+                            break;
+                        case 2:
+                            paziente.email = parola;
+                            break;
+                        case 5:
+                            paziente.Patologia = parola;
+                            break;
+                        case 6:
+
+                            paziente.Id = int.Parse(parola);
+                            break;
+                    }
+                    l++;
+                }
+                LSTpazienti.Add(paziente);
+            }
+        }
+
+        public void MediciDaFile()
+        {
+
+            string[] medicicsv = File.ReadAllLines("medici.csv");
+            foreach (string riga in medicicsv)
+            {
+
+                int l = 0;
+                Medico medico = new Medico();
+                for (int i = 0; i < 5; i++)
+                {
+
+                    string parola = "";
+                    while (riga[l].ToString() != ";")
+                    {
+                        parola += riga[l];
+                        l++;
+                    }
+
+                    switch (i)
+                    {
+                        case 0:
+                            medico.Nome = parola;
+                            break;
+                        case 1:
+                            medico.Cognome = parola;
+                            break;
+                        case 2:
+                            medico.email = parola;
+                            break;
+                        case 4:
+                            medico.Patologia = parola;
+                            break;
+                        case 3:
+                            medico.Id = int.Parse(parola);
+                            break;
+                    }
+                    l++;
+                }
+                LSTmedici.Add(medico);
+            }
+        }
+
+        public void PatologieDaFile()
+        {
+
+            string[] patologiecsv = File.ReadAllLines("patologie.csv");
+            foreach (string riga in patologiecsv)
+            {
+
+                int l = 0;
+                Patologia patologia = new Patologia();
+                for (int i = 0; i < 2; i++)
+                {
+
+                    string parola = "";
+                    while (riga[l].ToString() != ";")
+                    {
+                        parola += riga[l];
+                        l++;
+                    }
+
+                    switch (i)
+                    {
+                        case 1:
+                            patologia.Nome = parola;
+                            break;
+                        case 0:
+                            patologia.Id = int.Parse(parola);
+                            break;
+                    }
+                    l++;
+                }
+                LSTpatologie.Add(patologia);
+            }
+        }
+
+        public void SpecializzazioniDaFile()
+        {
+
+            string[] specializzazionicsv = File.ReadAllLines("specializzazioni.csv");
+            foreach (string riga in specializzazionicsv)
+            {
+
+                int l = 0;
+                Specializzazione specializzazione = new Specializzazione();
+                for (int i = 0; i < 2; i++)
+                {
+
+                    string parola = "";
+                    while (riga[l].ToString() != ";")
+                    {
+                        parola += riga[l];
+                        l++;
+                    }
+
+                    switch (i)
+                    {
+                        case 1:
+                            specializzazione.Nome = parola;
+                            break;
+                        case 0:
+                            specializzazione.Id = int.Parse(parola);
+                            break;
+                    }
+                    l++;
+                }
+                LSTspecializzazioni.Add(specializzazione);
+            }
+        }
+
+        public void AppuntamentiDaFile()
+        {
+
+            foreach (string riga in File.ReadAllLines("appuntamenti.csv"))
+            {
+
+                int l = 0;
+                Appuntamento appuntamenti = new Appuntamento();
+                for (int i = 0; i < 3; i++)
+                {
+
+                    string parola = "";
+                    while (riga[l].ToString() != ";")
+                    {
+                        parola += riga[l];
+                        l++;
+                    }
+
+                    switch (i)
+                    {
+                        case 0:
+                            appuntamenti.Data = DateTime.Parse(parola);
+                            break;
+                        case 1:
+                            appuntamenti.Paziente = TrovailPaziente(int.Parse(parola), 1);
+                            break;
+                        case 2:
+                            appuntamenti.Medico = TrovailPaziente(int.Parse(parola), 2);
+                            break;
+                    }
+                    l++;
+                }
+                LSTappuntamenti.Add(appuntamenti);
+            }
+        }
+
+        private string TrovailPaziente(int id, int type)
+        {
+            if (type == 1)
+            {
+                foreach (Paziente i in LSTpazienti)
+                {
+                    if (i.Id==id)
+                    {
+
+                        return i.Nome + " " + i.Cognome;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Medico i in LSTmedici)
+                {
+                    if (i.Id == id)
+                    {
+
+                        return i.Nome + " " + i.Cognome;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaricaDGV();
+        }
+
+        private void CaricaDGV()
+        {
+
+            LSTpazienti = new List<Paziente>();
+            LSTmedici = new List<Medico>();
+            LSTpatologie = new List<Patologia>();
+            LSTspecializzazioni = new List<Specializzazione>();
+            LSTappuntamenti= new List<Appuntamento>();
+
+            PazientiDaFile();
+            MediciDaFile();
+            PatologieDaFile();
+            SpecializzazioniDaFile();
+            AppuntamentiDaFile();
+
+            DGVPazienti.DataSource = null;
+            DGVMedici.DataSource = null;
+            DGVPatologie.DataSource = null;
+            DGVSpecializzazioni.DataSource = null;
+            DGVAppuntamenti.DataSource = null;
+
+            DGVPazienti.DataSource = LSTpazienti;
+            DGVMedici.DataSource = LSTmedici;
+            DGVPatologie.DataSource = LSTpatologie;
+            DGVSpecializzazioni.DataSource = LSTspecializzazioni;
+            DGVAppuntamenti.DataSource = LSTappuntamenti;
+
+            DGVMedici.Columns[0].Visible = false;
+            DGVMedici.Columns.Add((DataGridViewColumn)DGVMedici.Columns[1].Clone());
+            DGVMedici.Columns.RemoveAt(1);
+        }
+    }
+
+    class Appuntamento
+    {
+        public DateTime Data { get; set; }
+        public string Paziente { get; set; }
+        public string Medico { get; set; }
+    }
+
+    class Paziente
+    {
+
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string email { get; set; }
+        virtual public string Patologia { get; set; }
+        public int Id { get; set; }
+    }
+
+    class Medico : Paziente
+    {
+        
+        public override string Patologia { get => Specializzazione; set => Specializzazione = value; }
+        public string Specializzazione { get; set; }
+    }
+
+    class Patologia 
+    {
+        //private int[,] associazioni = new int[,] {
+        //    { 8, 4 },
+        //    { 8,9},
+        //    { 8,13},
+        //    { 10,7},
+        //    { 11,1},
+        //    { 11,6},
+        //    { 11,8},
+        //    { 11,16},
+        //    { 12,3},
+        //    { 13,5},
+        //    { 13,11},
+        //    { 13,18},
+        //    { 14,10},
+        //    { 14,14},
+        //    { 17,17},
+        //    { 19,19},
+        //    { 20,12},
+        //    { 21,15},
+        //    { 23,2}
+        //    };
+        public string Nome { get; set; }
+        public int Id { get; set; }
+    }
+
+    class Specializzazione 
+    {
+
+        public string Nome { get; set; }
+        public int Id { get; set; }
     }
 }
