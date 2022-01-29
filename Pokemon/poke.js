cfc43fbf0b38688691bb939a9742c2d29e5c89b4
salvy-greek <4079753@itisrossi.vi.it>
2022-01-29 21:43:25 +0100

    finito

--- Pokemon/poke.js
+++ Pokemon/poke.js
@@ -1,18 +1,19 @@
+"use strict";
 main();
 function main() {
-    for (var index = 0; index < 4; index++) {
+    for (let index = 0; index < 4; index++) {
         switch (index) {
             case 0:
-                takeit("charmander",index);
+                takeit("charmander", index);
                 break;
             case 1:
-                takeit("squirtle",index);
+                takeit("squirtle", index);
                 break;
             case 2:
-                takeit("turtwig",index);
+                takeit("turtwig", index);
                 break;
             case 3:
-                takeit("pikachu",index);
+                takeit("pikachu", index);
                 break;
             default:
                 break;
@@ -20,15 +21,35 @@ function main() {
     }
 }
 function takeit(nome, index) {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + nome)
-        .then(function (response) { return response.json(); })
-        .then(function (mon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+        .then(response => response.json())
+        .then((mon) => {
         if (index == 0)
-            addHTML("<h1 style=\"margin-left:10%\">CHOOSE YOUR FIRST POKEMON</h1><br><br>");
+            addHTML(`<h1 style="margin-left:10%">CHOOSE YOUR FIRST POKEMON</h1><br><br>`);
         if (index < 3)
-            addHTML(" \n                <div class=\"card\" style=\"display:inline-block;text-align: center;\">\n                        <img src=\"" + mon.sprites.front_default + "\" alt=\"Ma che ca...?\" style=\"width:40%;\">\n                        <br>\n                        <div class=\"container\" style=\"background-color: rgba(168, 144, 5, 0.534);\"><center>\n                            <b>Name: </b><br> " + mon.name + "<br><br>\n                            <b>Elemento: </b><br> " + mon.types[0].type.name + "<br><br>\n                            <b>Descrizione: </b><br> https://www.pokemon.com/it/pokedex/" + mon.name + "<br><br>\n                        </center></div>\n                    </div>&nbsp;\n                ");
+            addHTML(` 
+                <div class="card" style="display:inline-block;text-align: center;">
+                        <img src="${mon.sprites.front_default}" alt="Ma che ca...?" style="width:40%;">
+                        <br>
+                        <div class="container" style="background-color: rgba(168, 144, 5, 0.534);"><center>
+                            <b>Name: </b><br> ${mon.name}<br><br>
+                            <b>Elemento: </b><br> ${mon.types[0].type.name}<br><br>
+                            <b>Descrizione: </b><br> https://www.pokemon.com/it/pokedex/${mon.name}<br><br>
+                        </center></div>
+                    </div>&nbsp;
+                `);
         if (index == 3)
-            addHTML("\n                    <br><br><h1 style=\"margin-left:30%\">or</h1><br><br>\n                        <div class=\"card\" style=\"display:inline-block;text-align: center;margin-left:20%\">\n                            <img src=\"" + mon.sprites.front_default + "\" alt=\"Ma che ca...?\" style=\"width:40%;\">\n                            <br>\n                            <div class=\"container\" style=\"background-color: rgba(168, 144, 5, 0.534);\"><center>\n                                <b>Name: </b><br> " + mon.name + "<br><br>\n                                <b>Elemento: </b><br> " + mon.types[0].type.name + "<br><br>\n                                <b>Descrizione: </b><br> https://www.pokemon.com/it/pokedex/" + mon.name + "<br><br>\n                            </center></div>\n                        </div>");
+            addHTML(`
+                    <br><br><h1 style="margin-left:30%">or</h1><br><br>
+                        <div class="card" style="display:inline-block;text-align: center;margin-left:15%">
+                            <img src="${mon.sprites.front_default}" alt="Ma che ca...?" style="width:30%;">
+                            <br>
+                            <div class="container" style="background-color: rgba(168, 144, 5, 0.534);"><center>
+                                <b>Name: </b><br> ${mon.name}<br><br>
+                                <b>Elemento: </b><br> ${mon.types[0].type.name}<br><br>
+                                <b>Descrizione: </b><br> https://www.pokemon.com/it/pokedex/${mon.name}<br><br>
+                            </center></div>
+                        </div>`);
     });
 }
 function addHTML(what) {
