cfde9db093e6e1031714360bd6117ca3cf6ef64e
salvy-greek <4079753@itisrossi.vi.it>
2022-02-18 20:28:00 +0100

    finished

--- Exs Javascript_html/Slotmachine/script.js
+++ Exs Javascript_html/Slotmachine/script.js
@@ -1,57 +1,50 @@
-let slot1;
-let slot2;
-let slot3;
-
 let Main= async () => {
-    
-    slot1 = document.getElementById("slot1").src;
 
-    slot2 = document.getElementById("slot2").src;
-
-    slot3 = document.getElementById("slot3").src;
+    document.getElementById("win").style.display="none";
 
     for (let index = 0; index < 15; index++) {
     
-        slot1 = await Firstmove();
+        document.getElementById("slot1").src = await Firstmove();
 
-        slot2 = await Firstmove();
+        document.getElementById("slot2").src = await Firstmove();
 
-        slot3 = await Firstmove();
+        document.getElementById("slot3").src = await Firstmove();
     }
-    
+
     document.getElementById("win").style.display="block";
+    
     document.getElementById("win_img").src= Wincheck();
 }
 
 let Wincheck= () => {
 
+    let slot1 = document.getElementById("slot1").src;
+
+    let slot2 = document.getElementById("slot2").src;
+
+    let slot3 = document.getElementById("slot3").src;
+
     let count=1;
 
-    if (slot1 == slot2) {
-        
+    if (slot1 === slot2)
         count++;
-        
-        if(slot1==slot3){
-
-            count++;
-        }
-    }
-    else if(slot1==slot3){
-
+    if (slot1 === slot3)
         count++;
-    }
+    if (slot2 === slot3) 
+        count++;
 
     let win="";
         
     if(count > 1){
-        if(count == 3)
-            document.getElementById("jackpot").style.display="block";
+        if(count >= 3)
+            win= "./assets/jackpot.png";
         else 
-            document.getElementById("nice").style.display="block";
+            win= "./assets/nice.png";
     }
     else
-        document.getElementById("retry").style.display="block";
+        win= "./assets/retry.png";
 
+    console.log(slot1+ ", "+ slot2+", "+slot3+"= "+count)
     return win;
 }
 
@@ -59,7 +52,13 @@ let Firstmove=  () => {
 
     return new Promise((resolve) => {
 
-        n = Math.round(Math.random()*10)
+        let max = Math.round(Math.random()*10);
+        let min=0;
+
+        if(max>2)
+            min=max-3;
+
+        n = Math.round(Math.random() * (max - min) + min) //per aumentare le probabilità
 
         setTimeout(()=> resolve(`./assets/${n}.png`),100);
     })
