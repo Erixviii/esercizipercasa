cf187d605b33bc79d5a92855a9eea8783b4856ba
salvy-greek <4079753@itisrossi.vi.it>
2022-02-20 19:06:01 +0100

    finish

--- JSONListDente/prova verifica/prova verifica/Form2.cs
+++ JSONListDente/prova verifica/prova verifica/Form2.cs
@@ -21,19 +21,19 @@ namespace prova_verifica
         private void Form2_Load(object sender, EventArgs e)
         {
             List<Cantante> LSTCantanti= new List<Cantante>();
-            LSTCantanti.Add(new Cantante("0", "Daniel", "Colla", "italiano"));
-            LSTCantanti.Add(new Cantante("0", "Enrico", "FigBianc", "russo"));
-            LSTCantanti.Add(new Cantante("0", "Mattia", "SixMattinter", "albanese"));
-            LSTCantanti.Add(new Cantante("0", "Mattia", "SixMattinter", "italiano"));
-            LSTCantanti.Add(new Cantante("0", "Eric", "Erixviii.", "americano"));
-            LSTCantanti.Add(new Cantante("0", "Matteo", "MatGame04", "giapponese"));
+            LSTCantanti.Add(new Cantante("01", "Daniel", "Colla", "italiano"));
+            LSTCantanti.Add(new Cantante("02", "Enrico", "FigBianc", "russo"));
+            LSTCantanti.Add(new Cantante("03", "Mattia", "SixMattinter", "albanese"));
+            LSTCantanti.Add(new Cantante("04", "Matteo", "MatGame04", "giapponese"));
 
-            LSTCantanti[0].LSTCanzoni.Add(new Canzone("0", "italiano","20"));
-            LSTCantanti[0].LSTCanzoni.Add(new Canzone("0", "russo","20"));
-            LSTCantanti[1].LSTCanzoni.Add(new Canzone("0", "albanese","20"));
-            LSTCantanti[1].LSTCanzoni.Add(new Canzone("0", "italiano","20"));
-            LSTCantanti[2].LSTCanzoni.Add(new Canzone("0", "americano","20"));
-            LSTCantanti[2].LSTCanzoni.Add(new Canzone("0", "giapponese","20"));
+            LSTCantanti[0].LSTCanzoni.Add(new Canzone("10", "song1","2000"));
+            LSTCantanti[0].LSTCanzoni.Add(new Canzone("11", "song1", "2001"));
+            LSTCantanti[1].LSTCanzoni.Add(new Canzone("20", "song2", "2001"));
+            LSTCantanti[1].LSTCanzoni.Add(new Canzone("21", "song2", "2002"));
+            LSTCantanti[2].LSTCanzoni.Add(new Canzone("30", "song3", "2002"));
+            LSTCantanti[2].LSTCanzoni.Add(new Canzone("31", "song3", "2003"));
+            LSTCantanti[3].LSTCanzoni.Add(new Canzone("40", "song3", "2002"));
+            LSTCantanti[3].LSTCanzoni.Add(new Canzone("41", "song3", "2003"));
 
             BindingSource cantanti = new BindingSource
             {
@@ -46,11 +46,17 @@ namespace prova_verifica
 
             textBox1.DataBindings.Add(new Binding("Text", cantanti, "Nazionalità"));
 
-            listBox1.DataSource = comboBox1.DataSource;
+            BindingSource canzoni = new BindingSource
+            {
+                DataSource = comboBox1.DataSource,
+                DataMember = "LSTCanzoni"
+            };
+
+            listBox1.DataSource = canzoni;
             listBox1.DisplayMember = "NomeCanzone";
 
 
-            textBox2.DataBindings.Add(new Binding("Text", listBox1.DataSource , "AnnoProduzione"));
+            textBox2.DataBindings.Add(new Binding("Text", canzoni , "AnnoProduzione"));
 
         }
 
